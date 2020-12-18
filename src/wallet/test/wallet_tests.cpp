a674d039dc71ed9ac0637bf23147bec29e7f9185
Ivan Metlushko <metlushko@gmail.com>
2020-12-18 17:45:11 +0100

    refactor: move first run detection to client code
    Summary:
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/20773 | core#20773]]
    https://github.com/bitcoin/bitcoin/pull/20773/commits/e2a47ce08528dfb39c0340145c6977f6afd587f6
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, roqqit
    
    Reviewed By: #bitcoin_abc, roqqit
    
    Subscribers: roqqit
    
    Differential Revision: https://reviews.bitcoinabc.org/D17642

--- src/wallet/test/wallet_tests.cpp
+++ src/wallet/test/wallet_tests.cpp
@@ -565,8 +565,7 @@ public:
             wallet->SetLastBlockProcessed(chainman.ActiveHeight(),
                                           chainman.ActiveTip()->GetBlockHash());
         }
-        bool firstRun;
-        wallet->LoadWallet(firstRun);
+        wallet->LoadWallet();
         AddKey(*wallet, coinbaseKey);
         WalletRescanReserver reserver(*wallet);
         reserver.reserve();
