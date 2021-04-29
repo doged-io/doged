a72c9da022d2bfcbfa66c5b3e6560866873d1a12
Vasil Dimov <vd@FreeBSD.org>
2021-04-29 17:39:02 +0200

    Remove Sock::Release() and CloseSocket()
    Summary:
    - Sock::Release() is unused, thus remove it
    - CloseSocket() is only called from Sock::Reset(), so move the body of CloseSocket() inside Sock::Reset() and remove CloseSocket() - this helps to hide low level file descriptor sockets inside the Sock class.
    - Rename Sock::Reset() to Sock::Close() and make it private - to be used only in the destructor and in the Sock assignment operator. This simplifies the public API by removing one method from it.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/25428 | core#25428]]
    
    Test Plan: `ninja all check-all bitcoin-fuzzers`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19708

--- src/test/fuzz/util.h
+++ src/test/fuzz/util.h
@@ -50,10 +50,6 @@ public:
 
     SOCKET Get() const override;
 
-    SOCKET Release() override;
-
-    void Reset() override;
-
     ssize_t Send(const void *data, size_t len, int flags) const override;
 
     ssize_t Recv(void *buf, size_t len, int flags) const override;
