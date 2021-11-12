a4bacb3a48df2a4266054bc97799db325923e775
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2021-11-12 18:29:43 +0100

    test: remove unnecessary block rehashing prior to solving
    Summary:
    Solving a block involves continously rehashing it, i.e. any extra
    calls to rehash it before are not necessary and can be dropped.
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/23498 | core#23498]]
    
    Test Plan: `ninja check-functional`
    
    Reviewers: #bitcoin_abc, roqqit
    
    Reviewed By: #bitcoin_abc, roqqit
    
    Differential Revision: https://reviews.bitcoinabc.org/D17690

--- test/functional/feature_dersig.py
+++ test/functional/feature_dersig.py
@@ -65,7 +65,6 @@ class BIP66Test(BitcoinTestFramework):
         block_time = self.nodes[0].getblockheader(tip)["mediantime"] + 1
         block = create_block(int(tip, 16), create_coinbase(DERSIG_HEIGHT), block_time)
         block.nVersion = 2
-        block.rehash()
         block.solve()
 
         with self.nodes[0].assert_debug_log(
@@ -105,7 +104,6 @@ class BIP66Test(BitcoinTestFramework):
         # Now we verify that a block with this transaction is also invalid.
         block.vtx.append(spendtx)
         block.hashMerkleRoot = block.calc_merkle_root()
-        block.rehash()
         block.solve()
 
         with self.nodes[0].assert_debug_log(
@@ -121,7 +119,6 @@ class BIP66Test(BitcoinTestFramework):
         )
         block.vtx[1] = self.create_tx(self.coinbase_txids[1])
         block.hashMerkleRoot = block.calc_merkle_root()
-        block.rehash()
         block.solve()
 
         peer.send_and_ping(msg_block(block))
