2ce91f6e7bded1344463271ade7fa0d43f33de9d
Pieter Wuille <pieter@wuille.net>
2024-06-27 11:40:00 -0400

    random: get rid of GetRand by inlining
    Summary:
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/29625 | core#29625]]
    https://github.com/bitcoin/bitcoin/pull/29625/commits/ddc184d999d7e1a87efaf6bcb222186f0dcd87ec
    Depends on D19092
    
    Test Plan: `ninja all check-all`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19093

--- src/headerssync.cpp
+++ src/headerssync.cpp
@@ -39,7 +39,8 @@ HeadersSyncState::HeadersSyncState(NodeId id,
       m_chain_start(chain_start),
       m_minimum_required_work(minimum_required_work),
       m_current_chain_work(chain_start->nChainWork),
-      m_commit_offset(GetRand<unsigned>(HEADER_COMMITMENT_PERIOD)),
+      m_commit_offset(
+          FastRandomContext().randrange<unsigned>(HEADER_COMMITMENT_PERIOD)),
       m_last_header_received(chain_start_header),
       m_current_height(chain_start->nHeight) {
     // Estimate the number of blocks that could possibly exist on the peer's
