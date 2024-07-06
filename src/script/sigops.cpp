ddccaaacabd0a68a38763a59ee031ef1bc53b0ee
Tobias Ruck <ruck.tobias@gmail.com>
2024-07-07 00:50:03 +0200

    Add CountScriptSigOps
    This adds back in a simplified version of `CScript::GetSigOpCount` (originally removed in D6101), but keeps it separate in the function `CountScriptSigOps`.
    
    The function and Dogecoin's GetSigOpCount are identical in behavior, and OP_CHECKDATASIG(VERIFY) don't count as SigOps to avoid a fork.

---
+++ src/script/sigops.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2024 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <script/script.h>
+#include <script/sigops.h>
+
+uint32_t CountScriptSigOps(const CScript &script, SigOpCountMode mode) {
+    uint32_t nSigOps = 0;
+    CScript::const_iterator pc = script.begin();
+    opcodetype prevOpcode = OP_INVALIDOPCODE;
+
+    while (pc < script.end()) {
+        opcodetype opcode;
+        if (!script.GetOp(pc, opcode)) {
+            break;
+        }
+
+        switch (opcode) {
+            case OP_CHECKSIG:
+            case OP_CHECKSIGVERIFY:
+                nSigOps++;
+                break;
+
+            case OP_CHECKDATASIG:
+            case OP_CHECKDATASIGVERIFY:
+                // Dogecoin: These opcodes don't exist on Dogecoin and therefore
+                // don't count as sigops.
+                // It's important to not count them as unexecuted sigops still
+                // would count and could lead to a fork.
+                break;
+
+            case OP_CHECKMULTISIG:
+            case OP_CHECKMULTISIGVERIFY:
+                if (mode == SigOpCountMode::ACCURATE && prevOpcode >= OP_1 &&
+                    prevOpcode <= OP_16) {
+                    nSigOps += CScript::DecodeOP_N(prevOpcode);
+                } else {
+                    nSigOps += MAX_PUBKEYS_PER_MULTISIG;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        prevOpcode = opcode;
+    }
+
+    return nSigOps;
+}
---
+++ src/script/sigops.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+class CScript;
+
+enum class SigOpCountMode {
+    ACCURATE,
+    ESTIMATED,
+};
+
+uint32_t CountScriptSigOps(const CScript &script, SigOpCountMode mode);
