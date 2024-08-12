b709b185c4ed1be17d2c57be4704ba06bf0342c1
Roqqit <roqqit@proton.me>
2024-08-12 18:47:49 +0000

    [avalanche] Introduce StakeContenderId
    Summary: StakeContenderId will enable the node to poll for potential stake winners (as a new CInv type). This will further enable pre-consensus on staking rewards to ensure that nodes validate the same winner set even if some proofs are missing from some nodes' proof sets.
    
    Test Plan:
      ninja check-avalanche
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D16631

--- src/avalanche/test/stakingrewards_tests.cpp
+++ src/avalanche/test/stakingrewards_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <avalanche/avalanche.h>
 #include <avalanche/processor.h>
+#include <avalanche/stakecontender.h>
 #include <chainparams.h>
 #include <consensus/activation.h>
 #include <net_processing.h>
@@ -13,6 +14,8 @@
 #include <test/util/setup_common.h>
 
 #include <boost/test/unit_test.hpp>
+
+using namespace avalanche;
 
 struct StakingRewardsActivationTestingSetup : public TestingSetup {
     void checkStakingRewardsActivation(const std::string &net,
@@ -37,16 +40,41 @@ struct StakingRewardsActivationTestingSetup : public TestingSetup {
     }
 };
 
-BOOST_FIXTURE_TEST_SUITE(stakingrewards_tests,
-                         StakingRewardsActivationTestingSetup)
+BOOST_AUTO_TEST_SUITE(stakingrewards_tests)
 
-BOOST_AUTO_TEST_CASE(isstakingrewardsactivated) {
+BOOST_FIXTURE_TEST_CASE(isstakingrewardsactivated,
+                        StakingRewardsActivationTestingSetup) {
     checkStakingRewardsActivation("regtest", false);
     checkStakingRewardsActivation("test", false);
 
     Assert(m_node.args)->ForceSetArg("-avalanchestakingrewards", "1");
     checkStakingRewardsActivation("main", true);
     Assert(m_node.args)->ClearForcedArg("-avalanchestakingrewards");
+}
+
+BOOST_FIXTURE_TEST_CASE(stakecontender_computeid, TestChain100Setup) {
+    ChainstateManager &chainman = *Assert(m_node.chainman);
+    const CBlockIndex *chaintip =
+        WITH_LOCK(chainman.GetMutex(), return chainman.ActiveTip());
+
+    ProofId proofid1 = ProofId::fromHex(
+        "979dbc3b1351ee12f91f537e04e61fdf93a73d5ebfc317bccd12643b8be87b02");
+    BOOST_CHECK_EQUAL(
+        "1f40d5f66439e5ba739b4c03c33a2c699e01fde1ae69f3de2e90315f7d8fedca",
+        StakeContenderId(chaintip->GetAncestor(0)->GetBlockHash(), proofid1)
+            .ToString());
+
+    // Different prevblock should give different hash
+    BOOST_CHECK_EQUAL(
+        "1a3f06557dfb401aba65e3dcd90e2bd8fa505f74b4366f7d18a84bf3d843a749",
+        StakeContenderId(chaintip->GetBlockHash(), proofid1).ToString());
+
+    // So should a different proof id
+    ProofId proofid2 = ProofId::fromHex(
+        "e01bac293ed39e8d5e06214e7fe0bceb9646ef253ce501dcd7a475f802ab07f1");
+    BOOST_CHECK_EQUAL(
+        "5247e2093d593ff1301b6a1e636637b71ceb21943deae596885c1d4f501ebb93",
+        StakeContenderId(chaintip->GetBlockHash(), proofid2).ToString());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
