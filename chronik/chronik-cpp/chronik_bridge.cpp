aa492182dcaa0a3b38b9e2652a56dcc9ba6933a4
Lőrinc <pap.lorinc@gmail.com>
2024-09-08 21:57:11 +0200

    refactor: migrate bool GetCoin to return optional<Coin>
    Summary:
    > refactor: Remove unrealistic simulation state
    >
    > In non-test code the input coin is never mutated - it's either replaced or ignored.
    
    https://github.com/bitcoin/bitcoin/pull/30849/changes/e31bfb26c21f8b72f8c317e016d375862e27502e
    
    > interfaces: Change getUnspentOutput return type to avoid multiprocess segfault
    >
    > Coin serialize method segfaults if IsSpent condition is true. This caused
    > multiprocess code to segfault when serializing the Coin& output argument to of
    > the Node::getUnspentOutput method if the coin was not found. Segfault could be
    > triggered by double clicking and viewing transaction details in the GUI
    > transaction list.
    >
    > Fix this by replacing Coin& output argument with optional<Coin> return value to
    > avoid trying to serializing spent coins.
    
    https://github.com/bitcoin/bitcoin/pull/28721/changes/156f49d682ef025fb942c997a6c5475e18eef9cf
    
    The multiprocess thing is not relevant because we haven't started that backport yet, but the change is a good cleanup sense nonetheless. And it is required for the rest of [[https://github.com/bitcoin/bitcoin/pull/30849 | core#30849]]
    
    > refactor: Return optional of Coin in GetCoin
    
    https://github.com/bitcoin/bitcoin/pull/30849/changes/46dfbf169b49466de06dd16f7c23c6668419ef01
    https://github.com/bitcoin/bitcoin/pull/30849/changes/4feaa28728442b0fd29a677d2b170a05fdf967c0
    
    This is a backport of [[https://github.com/bitcoin/bitcoin/pull/30849 | core#30849]] and [[https://github.com/bitcoin/bitcoin/pull/28721 | core#28721]] (partial)
    
    Co-authored-by: TheCharlatan <seb.kung@gmail.com>
    Co-authored-by: Andrew Toth <andrewstoth@gmail.com>
    
    Test Plan: `ninja all check-all bitcoin-fuzzers`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D20091

--- chronik/chronik-cpp/chronik_bridge.cpp
+++ chronik/chronik-cpp/chronik_bridge.cpp
@@ -27,6 +27,8 @@
 #include <validation.h>
 #include <validationinterface.h>
 
+#include <optional>
+
 using common::TransactionErrorString;
 using node::TransactionError;
 
@@ -375,8 +377,8 @@ void ChronikBridge::lookup_spent_coins(
         // Remember if coin was already cached
         const bool had_cached = coins_cache.HaveCoinInCache(outpoint);
 
-        ::Coin coin;
-        if (!coin_view.GetCoin(outpoint, coin)) {
+        std::optional<::Coin> coin{coin_view.GetCoin(outpoint)};
+        if (!coin) {
             not_found.push_back(input.prev_out);
             continue;
         }
@@ -388,7 +390,7 @@ void ChronikBridge::lookup_spent_coins(
             // so we save one extra cache lookup here.
             coins_to_uncache.push_back(input.prev_out);
         }
-        input.coin = BridgeCoin(coin);
+        input.coin = BridgeCoin(std::move(*coin));
     }
 }
 
