bba782d8d57147f07a6fff93decdccb5e765b645
Tobias Ruck <ruck.tobias@gmail.com>
2024-11-21 15:50:46 +0100

    [guix] Use CMake's PROJECT_NAME for source package
    Summary: Currently this is hard-coded, but by adding the `print-project-name` target we can make it depend on cmake's project configuration.
    
    Test Plan: @bot guix-linux
    
    Reviewers: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D17179

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -244,31 +244,30 @@ case "$HOST" in
         ;;
 esac
 
+# Prepare for making the source_package
+# Also gather useful data from cmake: project name
+mkdir -p source_package
+pushd source_package
+cmake -GNinja .. \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
+    -DBUILD_BITCOIN_WALLET=OFF \
+    -DBUILD_BITCOIN_CHRONIK=OFF \
+    -DBUILD_DOGECASH_QT=OFF \
+    -DBUILD_BITCOIN_ZMQ=OFF \
+    -DENABLE_QRCODE=OFF \
+    -DENABLE_NATPMP=OFF \
+    -DENABLE_UPNP=OFF \
+    -DUSE_JEMALLOC=OFF \
+    -DENABLE_CLANG_TIDY=OFF \
+    -DENABLE_BIP70=OFF \
+    -DUSE_LINKER=
+PROJECT_NAME=$(ninja print-project-name)
+
 # Produce the source package if it does not already exist
-if ! ls "${OUTDIR_BASE}"/src/bitcoin-abc-*.tar.gz 1> /dev/null 2>&1; then
-    mkdir -p source_package
-    pushd source_package
-
-    cmake -GNinja .. \
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
-        -DBUILD_BITCOIN_WALLET=OFF \
-        -DBUILD_BITCOIN_CHRONIK=OFF \
-        -DBUILD_DOGECASH_QT=OFF \
-        -DBUILD_BITCOIN_ZMQ=OFF \
-        -DENABLE_QRCODE=OFF \
-        -DENABLE_NATPMP=OFF \
-        -DENABLE_UPNP=OFF \
-        -DUSE_JEMALLOC=OFF \
-        -DENABLE_CLANG_TIDY=OFF \
-        -DENABLE_BIP70=OFF \
-        -DUSE_LINKER=
-
+if ! ls "${OUTDIR_BASE}"/src/${PROJECT_NAME}-*.tar.gz 1> /dev/null 2>&1; then
     ninja package_source
-    SOURCEDIST=$(echo bitcoin-abc-*.tar.gz)
+    SOURCEDIST=$(echo ${PROJECT_NAME}-*.tar.gz)
     mv ${SOURCEDIST} ..
-
-    popd
-    rm -rf source_package
 
     DISTNAME=${SOURCEDIST//.tar.*/}
 
@@ -280,8 +279,12 @@ if ! ls "${OUTDIR_BASE}"/src/bitcoin-abc-*.tar.gz 1> /dev/null 2>&1; then
     rm -rf ${DISTNAME}
 else
     echo Skipping source package generation because it already exists.
-    DISTNAME=$(basename -s .tar.gz "${OUTDIR_BASE}"/src/bitcoin-abc-*.tar.gz)
+    DISTNAME=$(basename -s .tar.gz "${OUTDIR_BASE}"/src/${PROJECT_NAME}-*.tar.gz)
 fi
+
+# Remove temporary build dir
+popd
+rm -rf source_package
 
 mkdir -p "$OUTDIR"
 OUTDIR=$(realpath "${OUTDIR}")
