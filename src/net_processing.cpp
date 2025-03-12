65e7660ae02ef6f6556cea78584514f9cac76aa1
Roqqit <roqqit@proton.me>
2025-03-12 17:09:24 +0000

    [avalanche] Move contender cache to peer manager
    Summary:
    addStakeContender() being called from net_processing is not robust because there are various ways a proof may be introduced (via network, RPC, loaded at startup, etc). This results in the cache being unaware of proofs in various scenarios, eventually breaking contender voting. The robust solution is to put peerManager in charge of adding contenders to the cache whenever a proof is successfully registered. This is the first step to achieving that.
    
    This diff only moves the cache with minimal other changes (excepting some locking fixes). The pass-through API of the cache in peermanager is not ideal, however some of these can be refactored away in future diffs to reduce future maintenance overhead.
    
    There is no change in behavior.
    
    Test Plan:
      ninja check check-functional
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D17788

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -6957,7 +6957,6 @@ void PeerManagerImpl::ProcessMessage(
                             break;
                         }
                         case avalanche::VoteStatus::Finalized: {
-                            LOCK(cs_main);
                             m_avalanche->finalizeStakeContender(contenderId);
                             break;
                         }
@@ -9220,7 +9219,7 @@ bool PeerManagerImpl::ReceivedAvalancheProof(CNode &node, Peer &peer,
     saveProofIfStaker(node, proofid, nodeid);
 
     if (isStaker && m_opts.avalanche_staking_preconsensus) {
-        WITH_LOCK(cs_main, m_avalanche->addStakeContender(proof));
+        m_avalanche->addStakeContender(proof);
     }
 
     return true;
