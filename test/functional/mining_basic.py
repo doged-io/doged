ec27a061533c3fb80bd24cc1275150c69a339943
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-05-04 23:06:06 +0200

    test: remove bare CTransaction `.rehash()` calls
    Summary:
    Since the previous commit, CTransaction object calls to the
    methods `.rehash()` and `.calc_sha256()` are effectively no-ops
    if the returned value is not used, so we can just remove them.
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/32421 | core#32421]]
    https://github.com/bitcoin/bitcoin/pull/32421/commits/9b3dce24a3336a02563412b541a3fb2003e92506
    
    Depends on D18712
    
    Test Plan: `ninja check-functional-extended`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18713

--- test/functional/mining_basic.py
+++ test/functional/mining_basic.py
@@ -96,7 +96,6 @@ class MiningTest(BitcoinTestFramework):
         coinbase_tx = create_coinbase(height=next_height)
         # sequence numbers must not be max for nLockTime to have effect
         coinbase_tx.vin[0].nSequence = 2**32 - 2
-        coinbase_tx.rehash()
 
         block = CBlock()
         block.nVersion = tmpl["version"]
@@ -117,7 +116,6 @@ class MiningTest(BitcoinTestFramework):
         self.log.info("getblocktemplate: Test bad input hash for coinbase transaction")
         bad_block = copy.deepcopy(block)
         bad_block.vtx[0].vin[0].prevout.txid += 1
-        bad_block.vtx[0].rehash()
         assert_template(node, bad_block, "bad-cb-missing")
 
         self.log.info("submitblock: Test invalid coinbase transaction")
@@ -146,7 +144,6 @@ class MiningTest(BitcoinTestFramework):
         bad_block = copy.deepcopy(block)
         bad_tx = copy.deepcopy(bad_block.vtx[0])
         bad_tx.vin[0].prevout.txid = 255
-        bad_tx.rehash()
         bad_block.vtx.append(bad_tx)
         assert_template(node, bad_block, "bad-txns-inputs-missingorspent")
         assert_submitblock(bad_block, "bad-txns-inputs-missingorspent")
@@ -154,7 +151,6 @@ class MiningTest(BitcoinTestFramework):
         self.log.info("getblocktemplate: Test nonfinal transaction")
         bad_block = copy.deepcopy(block)
         bad_block.vtx[0].nLockTime = 2**32 - 1
-        bad_block.vtx[0].rehash()
         assert_template(node, bad_block, "bad-txns-nonfinal")
         assert_submitblock(bad_block, "bad-txns-nonfinal")
 
@@ -256,7 +252,6 @@ class MiningTest(BitcoinTestFramework):
 
         bad_block_lock = copy.deepcopy(block)
         bad_block_lock.vtx[0].nLockTime = 2**32 - 1
-        bad_block_lock.vtx[0].rehash()
         bad_block_lock.hashMerkleRoot = bad_block_lock.calc_merkle_root()
         bad_block_lock.solve()
         assert_equal(
