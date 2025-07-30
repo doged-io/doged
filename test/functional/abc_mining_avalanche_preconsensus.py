6db951216d89c7eea59b5b5ba1276dd4940933e3
Fabien <fabcien@gmail.com>
2025-07-30 15:21:33 +0200

    [avalanche] Fix a bug in the preconsensus mining test
    Summary: The test expects the quorum proofs to be finalized but it doesn't wait for it. As a result a single round of vote occurs. It has no impact on the test because the latest votes will complete their finalization as a side effect, but better fix the bug.
    
    Test Plan:
      ./test/functional/test_runner.py abc_mining_avalanche_preconsensus
    
    Reviewers: #bitcoin_abc, PiRK
    
    Reviewed By: #bitcoin_abc, PiRK
    
    Differential Revision: https://reviews.bitcoinabc.org/D18430

--- test/functional/abc_mining_avalanche_preconsensus.py
+++ test/functional/abc_mining_avalanche_preconsensus.py
@@ -71,8 +71,10 @@ class AvalancheMiningPreconsensusTest(BitcoinTestFramework):
                 for proofid in proofids
             )
 
-        finalize_proofs(node_non_preconsensus, quorum_non_preconsensus)
-        finalize_proofs(node_preconsensus, quorum_preconsensus)
+        self.wait_until(
+            lambda: finalize_proofs(node_non_preconsensus, quorum_non_preconsensus)
+        )
+        self.wait_until(lambda: finalize_proofs(node_preconsensus, quorum_preconsensus))
 
         # Get some coins so we can generate transactions
         wallet = MiniWallet(node_non_preconsensus)
