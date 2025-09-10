5b74da1d77cadf3ba87a7a2efa95704f3a51ae67
PiRK <pierrekn@gmail.com>
2025-09-10 20:45:16 +0200

    test: disable unit tests that are UB before C++20
    Summary:
    See https://en.cppreference.com/w/cpp/language/operator_arithmetic.html#Bitwise_shift_operators
    Before C++20:
    > For negative a, the behavior of a << b is undefined.
    
    Test Plan:
    With Clang + UBSAN:
    `ninja check`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D18601

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -3055,10 +3055,12 @@ template <typename T> void TestCheckedLeftShift() {
     if constexpr (std::is_signed_v<T>) {
         constexpr auto MIN{std::numeric_limits<T>::min()};
         // Negative input
-        BOOST_CHECK(CheckedLeftShift<T>(-1, 1) == -2);
-        BOOST_CHECK(CheckedLeftShift<T>((MIN >> 2), 1) == MIN / 2);
-        BOOST_CHECK(CheckedLeftShift<T>((MIN >> 1) + 1, 1) == MIN + 2);
-        BOOST_CHECK(CheckedLeftShift<T>(MIN >> 1, 1) == MIN);
+        // TODO: uncomment these tests after switching to C++20.
+        //       In C++17, left shifting negative values is undefined behavior.
+        // BOOST_CHECK(CheckedLeftShift<T>(-1, 1) == -2);
+        // BOOST_CHECK(CheckedLeftShift<T>((MIN >> 2), 1) == MIN / 2);
+        // BOOST_CHECK(CheckedLeftShift<T>((MIN >> 1) + 1, 1) == MIN + 2);
+        // BOOST_CHECK(CheckedLeftShift<T>(MIN >> 1, 1) == MIN);
         // Overflow negative
         BOOST_CHECK(!CheckedLeftShift<T>((MIN >> 1) - 1, 1));
         BOOST_CHECK(!CheckedLeftShift<T>(MIN >> 1, 2));
@@ -3094,10 +3096,12 @@ template <typename T> void TestSaturatingLeftShift() {
     if constexpr (std::is_signed_v<T>) {
         constexpr auto MIN{std::numeric_limits<T>::min()};
         // Negative input
-        BOOST_CHECK(SaturatingLeftShift<T>(-1, 1) == -2);
-        BOOST_CHECK(SaturatingLeftShift<T>((MIN >> 2), 1) == MIN / 2);
-        BOOST_CHECK(SaturatingLeftShift<T>((MIN >> 1) + 1, 1) == MIN + 2);
-        BOOST_CHECK(SaturatingLeftShift<T>(MIN >> 1, 1) == MIN);
+        // TODO: uncomment these tests after switching to C++20.
+        //       In C++17, left shifting negative values is undefined behavior.
+        // BOOST_CHECK(SaturatingLeftShift<T>(-1, 1) == -2);
+        // BOOST_CHECK(SaturatingLeftShift<T>((MIN >> 2), 1) == MIN / 2);
+        // BOOST_CHECK(SaturatingLeftShift<T>((MIN >> 1) + 1, 1) == MIN + 2);
+        // BOOST_CHECK(SaturatingLeftShift<T>(MIN >> 1, 1) == MIN);
         // Saturation negative
         BOOST_CHECK(SaturatingLeftShift<T>((MIN >> 1) - 1, 1) == MIN);
         BOOST_CHECK(SaturatingLeftShift<T>(MIN >> 1, 2) == MIN);
