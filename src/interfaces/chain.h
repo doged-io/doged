Lajandra/bitcoin

indexes, refactor: Remove CBlockIndex* uses in coinstatsindex LookUpOne function
This commit does not change behavior in any way.

src/interfaces/chain.h
@@ -38,6 +38,12 @@ namespace interfaces {
 class Handler;
 class Wallet;
 
+//! Hash/height pair to help track and identify blocks.
+struct BlockKey {
+    uint256 hash;
+    int height = -1;
+};
+
 //! Helper for findBlock to selectively return pieces of block data. If block is
 //! found, data will be returned by setting specified output variables. If block
 //! is not found, output variables will keep their previous values.

doged-io/doged

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/25494 | core#25494]]
https://github.com/bitcoin/bitcoin/pull/25494/commits/addb4f2af183a25ce4a6b6485b5b49575a2ba31b
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, roqqit
Reviewed By: #bitcoin_abc, roqqit
Differential Revision: https://reviews.bitcoinabc.org/D17721

src/interfaces/chain.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INTERFACES_CHAIN_H
 #define BITCOIN_INTERFACES_CHAIN_H
 
+#include <primitives/blockhash.h>
 #include <primitives/transaction.h>
 #include <primitives/txid.h>
 #include <util/settings.h> // For util::SettingsValue
@@ -28,7 +29,6 @@ class TxValidationState;
 
 enum class MemPoolRemovalReason;
 
-struct BlockHash;
 struct bilingual_str;
 struct CBlockLocator;
 namespace node {
@@ -43,6 +43,12 @@ namespace interfaces {
 
 class Handler;
 class Wallet;
+
+//! Hash/height pair to help track and identify blocks.
+struct BlockKey {
+    BlockHash hash;
+    int height = -1;
+};
 
 //! Helper for findBlock to selectively return pieces of block data.
 class FoundBlock {