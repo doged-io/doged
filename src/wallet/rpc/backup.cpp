Bitcoin-ABC/bitcoin-abc

rpc: Improve importdescriptor RPC error messages
Summary:
Particularly add more details in the case of pruning or assumeutxo.

This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/30909 | core#30909]]
https://github.com/bitcoin/bitcoin/pull/30909/commits/d73ae603d44f93e4d6c5116f235dd11a0bdbf89c

see D10159 for `GetRescanErrorMessage` and why this affects a wallet test that imports keys

Depends on D17926

Test Plan:
`ninja all check-all`

Also covered by another test in wallet_assumeutxo.py on my dev branch

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D17927

src/wallet/rpc/backup.cpp
@@ -1678,18 +1678,36 @@ static int64_t GetImportTimestamp(const UniValue &data, int64_t now) {
 
 static std::string GetRescanErrorMessage(const std::string &object,
                                          const int64_t objectTimestamp,
-                                         const int64_t blockTimestamp) {
-    return strprintf(
-        "Rescan failed for %s with creation timestamp %d. There was an error "
-        "reading a block from time %d, which is after or within %d seconds of "
-        "key creation, and could contain transactions pertaining to the %s. As "
-        "a result, transactions and coins using this %s may not appear in "
-        "the wallet. This error could be caused by pruning or data corruption "
-        "(see bitcoind log for details) and could be dealt with by downloading "
-        "and rescanning the relevant blocks (see -reindex and -rescan "
-        "options).",
-        object, objectTimestamp, blockTimestamp, TIMESTAMP_WINDOW, object,
-        object);
+                                         const int64_t blockTimestamp,
+                                         const bool have_pruned,
+                                         const bool has_assumed_valid_chain) {
+    std::string error_msg{
+        strprintf("Rescan failed for %s with creation timestamp %d. There "
+                  "was an error reading a block from time %d, which is after "
+                  "or within %d seconds of key creation, and could contain "
+                  "transactions pertaining to the %s. As a result, "
+                  "transactions and coins using this %s may not appear "
+                  "in the wallet. ",
+                  object, objectTimestamp, blockTimestamp, TIMESTAMP_WINDOW,
+                  object, object)};
+    if (have_pruned) {
+        error_msg += strprintf(
+            "This error could be caused by pruning or data corruption "
+            "(see bitcoind log for details) and could be dealt with by "
+            "downloading and rescanning the relevant blocks (see -reindex "
+            "option and rescanblockchain RPC).");
+    } else if (has_assumed_valid_chain) {
+        error_msg += strprintf(
+            "This error is likely caused by an in-progress assumeutxo "
+            "background sync. Check logs or getchainstates RPC for assumeutxo "
+            "background sync progress and try again later.");
+    } else {
+        error_msg += strprintf(
+            "This error could potentially be caused by data corruption. If "
+            "the issue persists you may want to reindex (see -reindex "
+            "option).");
+    }
+    return error_msg;
 }
 
 RPCHelpMan importmulti() {
@@ -1959,7 +1977,10 @@ RPCHelpMan importmulti() {
                                     RPC_MISC_ERROR,
                                     GetRescanErrorMessage(
                                         "key", GetImportTimestamp(request, now),
-                                        scannedTime - TIMESTAMP_WINDOW - 1)));
+                                        scannedTime - TIMESTAMP_WINDOW - 1,
+                                        pwallet->chain().havePruned(),
+                                        pwallet->chain()
+                                            .hasAssumedValidChain())));
                             response.push_back(std::move(result));
                         }
                         ++i;
@@ -2354,7 +2375,10 @@ RPCHelpMan importdescriptors() {
                                     GetRescanErrorMessage(
                                         "descriptor",
                                         GetImportTimestamp(request, now),
-                                        scanned_time - TIMESTAMP_WINDOW - 1)));
+                                        scanned_time - TIMESTAMP_WINDOW - 1,
+                                        pwallet->chain().havePruned(),
+                                        pwallet->chain()
+                                            .hasAssumedValidChain())));
                             response.push_back(std::move(result));
                         }
                     }

doged-io/doged

src/wallet/rpc/backup.cpp
@@ -1678,19 +1678,36 @@ static int64_t GetImportTimestamp(const UniValue &data, int64_t now) {
 
 static std::string GetRescanErrorMessage(const std::string &object,
                                          const int64_t objectTimestamp,
-                                         const int64_t blockTimestamp) {
-    return strprintf(
-        "Rescan failed for %s with creation timestamp %d. There was an error "
-        "reading a block from time %d, which is after or within %d seconds of "
-        "key creation, and could contain transactions pertaining to the %s. As "
-        "a result, transactions and coins using this %s may not appear in "
-        "the wallet. This error could be caused by pruning or data corruption "
-        "(see doged log for details) and could be dealt with by "
-        "downloading "
-        "and rescanning the relevant blocks (see -reindex and -rescan "
-        "options).",
-        object, objectTimestamp, blockTimestamp, TIMESTAMP_WINDOW, object,
-        object);
+                                         const int64_t blockTimestamp,
+                                         const bool have_pruned,
+                                         const bool has_assumed_valid_chain) {
+    std::string error_msg{
+        strprintf("Rescan failed for %s with creation timestamp %d. There "
+                  "was an error reading a block from time %d, which is after "
+                  "or within %d seconds of key creation, and could contain "
+                  "transactions pertaining to the %s. As a result, "
+                  "transactions and coins using this %s may not appear "
+                  "in the wallet. ",
+                  object, objectTimestamp, blockTimestamp, TIMESTAMP_WINDOW,
+                  object, object)};
+    if (have_pruned) {
+        error_msg += strprintf(
+            "This error could be caused by pruning or data corruption "
+            "(see doged log for details) and could be dealt with by "
+            "downloading and rescanning the relevant blocks (see -reindex "
+            "option and rescanblockchain RPC).");
+    } else if (has_assumed_valid_chain) {
+        error_msg += strprintf(
+            "This error is likely caused by an in-progress assumeutxo "
+            "background sync. Check logs or getchainstates RPC for assumeutxo "
+            "background sync progress and try again later.");
+    } else {
+        error_msg += strprintf(
+            "This error could potentially be caused by data corruption. If "
+            "the issue persists you may want to reindex (see -reindex "
+            "option).");
+    }
+    return error_msg;
 }
 
 RPCHelpMan importmulti() {
@@ -1960,7 +1977,10 @@ RPCHelpMan importmulti() {
                                     RPC_MISC_ERROR,
                                     GetRescanErrorMessage(
                                         "key", GetImportTimestamp(request, now),
-                                        scannedTime - TIMESTAMP_WINDOW - 1)));
+                                        scannedTime - TIMESTAMP_WINDOW - 1,
+                                        pwallet->chain().havePruned(),
+                                        pwallet->chain()
+                                            .hasAssumedValidChain())));
                             response.push_back(std::move(result));
                         }
                         ++i;
@@ -2355,7 +2375,10 @@ RPCHelpMan importdescriptors() {
                                     GetRescanErrorMessage(
                                         "descriptor",
                                         GetImportTimestamp(request, now),
-                                        scanned_time - TIMESTAMP_WINDOW - 1)));
+                                        scanned_time - TIMESTAMP_WINDOW - 1,
+                                        pwallet->chain().havePruned(),
+                                        pwallet->chain()
+                                            .hasAssumedValidChain())));
                             response.push_back(std::move(result));
                         }
                     }