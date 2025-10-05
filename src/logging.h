04db7718b477e73b83364bf5d68d99c0584130f9
Alexandre Guillioud <alexandre@guillioud.com>
2025-10-05 14:22:00 +0100

    add stratum JSON-RPC protocol message types

--- src/logging.h
+++ src/logging.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Bitcoin developers
+// Copyright (c) 2025 Tobias Ruck and Alexandre Guillioud
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
