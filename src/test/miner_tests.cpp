f1444f51d0ca3ea5b1423e6c4061de590111a6e2
PiRK <pierrekn@gmail.com>
2026-01-07 16:17:24 +0100

    scripted-diff: [test] Use g_rng/m_rng directly
    Summary:
    ```
    -BEGIN VERIFY SCRIPT-
    
     # Use m_rng in unit test files
     ren() { sed -i "s:\<$1\>:$2:g" $( git grep -l "$1" src/test/*.cpp src/wallet/test/*.cpp src/test/util/setup_common.cpp  src/pow/test/*.cpp src/avalanche/test/*.cpp) ; }
     ren InsecureRand32                m_rng.rand32
     ren InsecureRand160               m_rng.rand160
     ren InsecureRand256               m_rng.rand256
     ren InsecureRandBits              m_rng.randbits
     ren InsecureRandRange             m_rng.randrange
     ren InsecureRandBool              m_rng.randbool
     ren g_insecure_rand_ctx           m_rng
     ren g_insecure_rand_ctx_temp_path g_rng_temp_path
    
    -END VERIFY SCRIPT-
    ```
    
    This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/30571 | core#30571]]
    https://github.com/bitcoin/bitcoin/pull/30571/commits/fa0fe08eca48064b2a42789571fea017e455d820
    Depends on D19258
    
    Test Plan: `ninja check`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19259

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -388,7 +388,7 @@ void MinerTestingSetup::TestBasicMining(
     while (m_node.chainman->ActiveHeight() < 209999) {
         CBlockIndex *prev = m_node.chainman->ActiveTip();
         CBlockIndex *next = new CBlockIndex();
-        next->phashBlock = new BlockHash(InsecureRand256());
+        next->phashBlock = new BlockHash(m_rng.rand256());
         m_node.chainman->ActiveChainstate().CoinsTip().SetBestBlock(
             next->GetBlockHash());
         next->pprev = prev;
@@ -402,7 +402,7 @@ void MinerTestingSetup::TestBasicMining(
     while (m_node.chainman->ActiveHeight() < 210000) {
         CBlockIndex *prev = m_node.chainman->ActiveTip();
         CBlockIndex *next = new CBlockIndex();
-        next->phashBlock = new BlockHash(InsecureRand256());
+        next->phashBlock = new BlockHash(m_rng.rand256());
         m_node.chainman->ActiveChainstate().CoinsTip().SetBestBlock(
             next->GetBlockHash());
         next->pprev = prev;
