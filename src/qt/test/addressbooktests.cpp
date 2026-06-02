Bitcoin-ABC/bitcoin-abc

qt6, test: Handle deprecated code
Summary:
> qt6, test: Use `qWarning()` instead of `QWARN()` macro
>
> The `QWARN()` macro internally uses `QTest::qWarn()`, which has been
> deprecated since Qt 6.3. Replacing it with `qWarning()` ensures
> compatibility across all Qt versions.

> qt6, test: Handle deprecated `QVERIFY_EXCEPTION_THROWN`
>
> This change ensures compatibility across all supported Qt versions.
>
> Co-Authored-By: João Barbosa <joao.paulo.barbosa@gmail.com>

This is a backport of [[https://github.com/bitcoin-core/gui/pull/839 | core-gui#839]]
Depends on D18199

Test Plan:
`ninja check`
With Qt6.4, verify the lack of deprecation warnings when compiling

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D18200

src/qt/test/addressbooktests.cpp
@@ -219,10 +219,11 @@ void AddressBookTests::addressBookTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN("Skipping AddressBookTests on mac build with 'minimal' platform "
-              "set due to Qt bugs. To run AppTests, invoke with "
-              "'QT_QPA_PLATFORM=cocoa test_bitcoin-qt' on mac, or else use a "
-              "linux or windows build.");
+        qwarning()
+            << "Skipping AddressBookTests on mac build with 'minimal' platform "
+               "set due to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_bitcoin-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif

src/qt/test/apptests.cpp
@@ -68,9 +68,11 @@ void AppTests::appTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN("Skipping AppTests on mac build with 'minimal' platform set due "
-              "to Qt bugs. To run AppTests, invoke with 'QT_QPA_PLATFORM=cocoa "
-              "test_bitcoin-qt' on mac, or else use a linux or windows build.");
+        qWarning()
+            << "Skipping AppTests on mac build with 'minimal' platform set due "
+               "to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_bitcoin-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif

src/qt/test/wallettests.cpp
@@ -304,10 +304,11 @@ void WalletTests::walletTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN(
-            "Skipping WalletTests on mac build with 'minimal' platform set due "
-            "to Qt bugs. To run AppTests, invoke with 'QT_QPA_PLATFORM=cocoa "
-            "test_bitcoin-qt' on mac, or else use a linux or windows build.");
+        qwarning()
+            << "Skipping WalletTests on mac build with 'minimal' platform set "
+               "due to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_bitcoin-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif

doged-io/doged

src/qt/test/addressbooktests.cpp
@@ -219,10 +219,11 @@ void AddressBookTests::addressBookTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN("Skipping AddressBookTests on mac build with 'minimal' platform "
-              "set due to Qt bugs. To run AppTests, invoke with "
-              "'QT_QPA_PLATFORM=cocoa test_doge-qt' on mac, or else use a "
-              "linux or windows build.");
+        qwarning()
+            << "Skipping AddressBookTests on mac build with 'minimal' platform "
+               "set due to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_doge-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif

src/qt/test/apptests.cpp
@@ -68,9 +68,11 @@ void AppTests::appTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN("Skipping AppTests on mac build with 'minimal' platform set due "
-              "to Qt bugs. To run AppTests, invoke with 'QT_QPA_PLATFORM=cocoa "
-              "test_doge-qt' on mac, or else use a linux or windows build.");
+        qWarning()
+            << "Skipping AppTests on mac build with 'minimal' platform set due "
+               "to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_doge-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif

src/qt/test/wallettests.cpp
@@ -304,10 +304,11 @@ void WalletTests::walletTests() {
         // framework when it tries to look up unimplemented cocoa functions,
         // and fails to handle returned nulls
         // (https://bugreports.qt.io/browse/QTBUG-49686).
-        QWARN(
-            "Skipping WalletTests on mac build with 'minimal' platform set due "
-            "to Qt bugs. To run AppTests, invoke with 'QT_QPA_PLATFORM=cocoa "
-            "test_doge-qt' on mac, or else use a linux or windows build.");
+        qwarning()
+            << "Skipping WalletTests on mac build with 'minimal' platform set "
+               "due to Qt bugs. To run AppTests, invoke with "
+               "'QT_QPA_PLATFORM=cocoa test_doge-qt' on mac, or else use a "
+               "linux or windows build.";
         return;
     }
 #endif