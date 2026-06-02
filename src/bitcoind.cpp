Bitcoin-ABC/bitcoin-abc

Deduplicate bitcoind and bitcoin-qt init code
Summary:
> Remove double newlines after some init errors
>
> Some InitError calls had trailing \n characters, causing double newlines in
> error output. After this change InitError calls consistently output one newline
> instead of two. Appearance of messages in the GUI does not seem to be affected.
> Can be tested with:
>
>   src/bitcoind -regtest -datadir=noexist
>   src/qt/bitcoin-qt -regtest -datadir=noexist

> Add InitError(error, details) overload
>
> This is only used in the current PR to avoid ugly
> `strprintf(Untranslated("%s:\n%s"), str, MakeUnorderedList(details)`
> boilerplate in init code. But in the future the function could be extended and
> more widely used to include more details in GUI error messages or display them
> in a more readable way, see code comment.

> Deduplicate bitcoind and bitcoin-qt init code
>
> Add common InitConfig function to deduplicate bitcoind and bitcoin-qt code
> reading config files and creating the datadir.
>
> There are a few minor changes in behavior:
>
> - In bitcoin-qt, when there is a problem reading the configuration file, the
>   GUI error text has changed from "Error: Cannot parse configuration file:" to
>   "Error reading configuration file:" to be consistent with bitcoind.
> - In bitcoind, when there is a problem reading the settings.json file, the
>   error text has changed from "Failed loading settings file" to "Settings
>   file could not be read" to be consistent with bitcoin-qt.
> - In bitcoind, when there is a problem writing the settings.json file, the
>   error text has changed from "Failed saving settings file" to "Settings file
>   could not be written" to be consistent with bitcoin-qt.
> - In bitcoin-qt, if there datadir is not accessible (e.g. no permission to read),
>   there is an normal error dialog showing "Error: filesystem error: status:
>   Permission denied [.../settings.json]", instead of an uncaught exception

This is a backport of [[https://git

src/bitcoind.cpp
@@ -10,6 +10,7 @@
 #include <chainparams.h>
 #include <clientversion.h>
 #include <common/args.h>
+#include <common/init.h>
 #include <common/system.h>
 #include <compat.h>
 #include <config.h>
@@ -151,7 +152,7 @@ static bool AppInit(int argc, char *argv[]) {
     std::string error;
     if (!args.ParseParameters(argc, argv, error)) {
         return InitError(Untranslated(
-            strprintf("Error parsing command line arguments: %s\n", error)));
+            strprintf("Error parsing command line arguments: %s", error)));
     }
 
     // Process help and version before taking care about datadir
@@ -182,21 +183,8 @@ static bool AppInit(int argc, char *argv[]) {
     TokenPipeEnd daemon_ep;
 #endif
     try {
-        if (!CheckDataDirOption(args)) {
-            return InitError(Untranslated(
-                strprintf("Specified data directory \"%s\" does not exist.\n",
-                          args.GetArg("-datadir", ""))));
-        }
-        if (!args.ReadConfigFiles(error, true)) {
-            return InitError(Untranslated(
-                strprintf("Error reading configuration file: %s\n", error)));
-        }
-        // Check for -chain, -testnet or -regtest parameter (Params() calls are
-        // only valid after this clause)
-        try {
-            SelectParams(args.GetChainType());
-        } catch (const std::exception &e) {
-            return InitError(Untranslated(strprintf("%s\n", e.what())));
+        if (auto err = common::InitConfig(args)) {
+            return InitError(err->message, err->details);
         }
 
         // Make sure we create the net-specific data directory early on: if it
@@ -214,14 +202,9 @@ static bool AppInit(int argc, char *argv[]) {
             if (!IsSwitchChar(argv[i][0])) {
                 return InitError(Untranslated(
                     strprintf("Command line contains unexpected token '%s', "
-                              "see bitcoind -h for a list of options.\n",
+                              "see bitcoind -h for a list of options.",
                               argv[i])));
             }
-        }
-
-        if (!args.InitSettings(error)) {
-            InitError(Untranslated(error));
-            return false;
         }
 
         // -server defaults to true for bitcoind but not for the GUI so do this
@@ -268,7 +251,7 @@ static bool AppInit(int argc, char *argv[]) {
                 case -1:
                     // Error happened.
                     return InitError(Untranslated(strprintf(
-                        "fork_daemon() failed: %s\n", SysErrorString(errno))));
+                        "fork_daemon() failed: %s", SysErrorString(errno))));
                 default: {
                     // Parent: wait and exit.
                     int token = daemon_ep.TokenRead();
@@ -285,7 +268,7 @@ static bool AppInit(int argc, char *argv[]) {
             }
 #else
             return InitError(Untranslated(
-                "-daemon is not supported on this operating system\n"));
+                "-daemon is not supported on this operating system"));
 #endif // HAVE_DECL_FORK
         }
 

doged-io/doged

src/bitcoind.cpp
@@ -10,6 +10,7 @@
 #include <chainparams.h>
 #include <clientversion.h>
 #include <common/args.h>
+#include <common/init.h>
 #include <common/system.h>
 #include <compat.h>
 #include <config.h>
@@ -151,7 +152,7 @@ static bool AppInit(int argc, char *argv[]) {
     std::string error;
     if (!args.ParseParameters(argc, argv, error)) {
         return InitError(Untranslated(
-            strprintf("Error parsing command line arguments: %s\n", error)));
+            strprintf("Error parsing command line arguments: %s", error)));
     }
 
     // Process help and version before taking care about datadir
@@ -182,21 +183,8 @@ static bool AppInit(int argc, char *argv[]) {
     TokenPipeEnd daemon_ep;
 #endif
     try {
-        if (!CheckDataDirOption(args)) {
-            return InitError(Untranslated(
-                strprintf("Specified data directory \"%s\" does not exist.\n",
-                          args.GetArg("-datadir", ""))));
-        }
-        if (!args.ReadConfigFiles(error, true)) {
-            return InitError(Untranslated(
-                strprintf("Error reading configuration file: %s\n", error)));
-        }
-        // Check for -chain, -testnet or -regtest parameter (Params() calls are
-        // only valid after this clause)
-        try {
-            SelectParams(args.GetChainType());
-        } catch (const std::exception &e) {
-            return InitError(Untranslated(strprintf("%s\n", e.what())));
+        if (auto err = common::InitConfig(args)) {
+            return InitError(err->message, err->details);
         }
 
         // Make sure we create the net-specific data directory early on: if it
@@ -214,14 +202,9 @@ static bool AppInit(int argc, char *argv[]) {
             if (!IsSwitchChar(argv[i][0])) {
                 return InitError(Untranslated(
                     strprintf("Command line contains unexpected token '%s', "
-                              "see doged -h for a list of options.\n",
+                              "see doged -h for a list of options.",
                               argv[i])));
             }
-        }
-
-        if (!args.InitSettings(error)) {
-            InitError(Untranslated(error));
-            return false;
         }
 
         // -server defaults to true for doged but not for the GUI so do this
@@ -268,7 +251,7 @@ static bool AppInit(int argc, char *argv[]) {
                 case -1:
                     // Error happened.
                     return InitError(Untranslated(strprintf(
-                        "fork_daemon() failed: %s\n", SysErrorString(errno))));
+                        "fork_daemon() failed: %s", SysErrorString(errno))));
                 default: {
                     // Parent: wait and exit.
                     int token = daemon_ep.TokenRead();
@@ -285,7 +268,7 @@ static bool AppInit(int argc, char *argv[]) {
             }
 #else
             return InitError(Untranslated(
-                "-daemon is not supported on this operating system\n"));
+                "-daemon is not supported on this operating system"));
 #endif // HAVE_DECL_FORK
         }
 