Bitcoin-ABC/bitcoin-abc

test: rename CBlockHeader .hash -> .hash_hex for consistency
Summary:
Note that we unfortunately can't use a scripted diff here, as the
`.hash` symbol is also used for other instances (e.g. CInv)

This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/32868 | core#32868]]
https://github.com/bitcoin/bitcoin/pull/32868/commits/2118301d77c20e31e8c280ed3bf3c7d67c07d3ca.

Depends on D18740

Test Plan:
`ninja check-functional-extended`
chronik-client-integration-tests
build-chronik-plugins

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D18741

test/functional/feature_cltv.py
@@ -107,7 +107,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
         self.log.info("Test that blocks must now be at least version 4")
         tip = block.hash_int
@@ -116,7 +116,7 @@ class BIP65Test(BitcoinTestFramework):
         block.solve()
 
         with self.nodes[0].assert_debug_log(
-            expected_msgs=[f"{block.hash}, bad-version(0x00000003)"]
+            expected_msgs=[f"{block.hash_hex}, bad-version(0x00000003)"]
         ):
             peer.send_and_ping(msg_block(block))
             assert_equal(int(self.nodes[0].getbestblockhash(), 16), tip)
@@ -142,7 +142,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
         # We show that this tx is invalid due to CLTV by getting it
         # rejected from the mempool for exactly that reason.
@@ -161,7 +161,7 @@ class BIP65Test(BitcoinTestFramework):
             ),
         )
 
-        tip = block.hash
+        tip = block.hash_hex
         block_time += 1
         block = create_block(
             block.hash_int,
@@ -173,7 +173,7 @@ class BIP65Test(BitcoinTestFramework):
         block.solve()
 
         with self.nodes[0].assert_debug_log(
-            expected_msgs=[f"ConnectBlock {block.hash} failed, blk-bad-inputs"]
+            expected_msgs=[f"ConnectBlock {block.hash_hex} failed, blk-bad-inputs"]
         ):
             peer.send_and_ping(msg_block(block))
             assert_equal(self.nodes[0].getbestblockhash(), tip)
@@ -206,7 +206,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is now valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
 
 if __name__ == "__main__":

doged-io/doged

test/functional/feature_cltv.py
@@ -108,7 +108,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
         self.log.info("Test that blocks must now be at least version 4")
         tip = block.hash_int
@@ -117,7 +117,7 @@ class BIP65Test(BitcoinTestFramework):
         block.solve()
 
         with self.nodes[0].assert_debug_log(
-            expected_msgs=[f"{block.hash}, bad-version(0x00620003)"]
+            expected_msgs=[f"{block.hash_hex}, bad-version(0x00620003)"]
         ):
             peer.send_and_ping(msg_block(block))
             assert_equal(int(self.nodes[0].getbestblockhash(), 16), tip)
@@ -143,7 +143,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
         # We show that this tx is invalid due to CLTV by getting it
         # rejected from the mempool for exactly that reason.
@@ -162,7 +162,7 @@ class BIP65Test(BitcoinTestFramework):
             ),
         )
 
-        tip = block.hash
+        tip = block.hash_hex
         block_time += 1
         block = create_block(
             block.hash_int,
@@ -174,7 +174,7 @@ class BIP65Test(BitcoinTestFramework):
         block.solve()
 
         with self.nodes[0].assert_debug_log(
-            expected_msgs=[f"ConnectBlock {block.hash} failed, blk-bad-inputs"]
+            expected_msgs=[f"ConnectBlock {block.hash_hex} failed, blk-bad-inputs"]
         ):
             peer.send_and_ping(msg_block(block))
             assert_equal(self.nodes[0].getbestblockhash(), tip)
@@ -207,7 +207,7 @@ class BIP65Test(BitcoinTestFramework):
 
         peer.send_and_ping(msg_block(block))
         # This block is now valid
-        assert_equal(self.nodes[0].getbestblockhash(), block.hash)
+        assert_equal(self.nodes[0].getbestblockhash(), block.hash_hex)
 
 
 if __name__ == "__main__":