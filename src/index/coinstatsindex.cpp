bitcoin-ghost/bitcoin-core-ladder-script

refactor: Avoid integer overflow in ApplyStats when activating snapshot

src/index/coinstatsindex.cpp
@@ -321,7 +321,7 @@ bool CoinStatsIndex::LookUpStats(const CBlockIndex* block_index, CCoinsStats& co
     coins_stats.hashSerialized = entry.muhash;
     coins_stats.nTransactionOutputs = entry.transaction_output_count;
     coins_stats.nBogoSize = entry.bogo_size;
-    coins_stats.nTotalAmount = entry.total_amount;
+    coins_stats.total_amount = entry.total_amount;
     coins_stats.total_subsidy = entry.total_subsidy;
     coins_stats.total_unspendable_amount = entry.total_unspendable_amount;
     coins_stats.total_prevout_spent_amount = entry.total_prevout_spent_amount;

doged-io/doged

Summary:
Similar to previous commit, add a CheckedAddAmount that returns nullopt instead of overflowing. Use it in gettxoutsetinfo
This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/23411 | core#23411]]
https://github.com/bitcoin/bitcoin/pull/23411/commits/fa996c58e8a31ebe610d186cef408b6dd3b385a8
Depends on D19111
Test Plan: `ninja all check-all`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19112

src/index/coinstatsindex.cpp
@@ -359,7 +359,7 @@ CoinStatsIndex::LookUpStats(const CBlockIndex *block_index) const {
     stats.hashSerialized = entry.muhash;
     stats.nTransactionOutputs = entry.transaction_output_count;
     stats.nBogoSize = entry.bogo_size;
-    stats.nTotalAmount = entry.total_amount;
+    stats.total_amount = entry.total_amount;
     stats.total_subsidy = entry.total_subsidy;
     stats.total_unspendable_amount = entry.total_unspendable_amount;
     stats.total_prevout_spent_amount = entry.total_prevout_spent_amount;