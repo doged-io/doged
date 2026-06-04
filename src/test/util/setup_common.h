bitcoin-ghost/bitcoin-core-ladder-script

refactor: Make uint256_tests no longer use deprecated BOOST_CHECK()

src/test/util/setup_common.h
@@ -24,6 +24,7 @@
 #include <type_traits>
 #include <vector>
 
+class arith_uint256;
 class CFeeRate;
 class Chainstate;
 class FastRandomContext;
@@ -234,7 +235,9 @@ std::unique_ptr<T> MakeNoLogFileContext(const ChainType chain_type = ChainType::
 
 CBlock getBlock13b8a();
 
-// define an implicit conversion here so that uint256 may be used directly in BOOST_CHECK_*
+// Make types usable in BOOST_CHECK_*
+std::ostream& operator<<(std::ostream& os, const arith_uint256& num);
+std::ostream& operator<<(std::ostream& os, const uint160& num);
 std::ostream& operator<<(std::ostream& os, const uint256& num);
 
 /**

doged-io/doged

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/30436 | core#30436]]
https://github.com/bitcoin/bitcoin/pull/30436/commits/f11f816800ac520064a1e96871d0b4cc9601ced7
Test Plan: `ninja check`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D18870

src/test/util/setup_common.h
@@ -32,6 +32,7 @@
 #include <type_traits>
 #include <vector>
 
+class arith_uint256;
 class CFeeRate;
 class Config;
 class FastRandomContext;
@@ -333,8 +334,9 @@ struct TestMemPoolEntryHelper {
 
 enum class ScriptError;
 
-// define implicit conversions here so that these types may be used in
-// BOOST_*_EQUAL
+// Make types usable in BOOST_CHECK_*
+std::ostream &operator<<(std::ostream &os, const arith_uint256 &num);
+std::ostream &operator<<(std::ostream &os, const uint160 &num);
 std::ostream &operator<<(std::ostream &os, const uint256 &num);
 std::ostream &operator<<(std::ostream &os, const ScriptError &err);
 