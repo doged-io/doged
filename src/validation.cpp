bitcoin-ghost/bitcoin-core-ladder-script

Make BufferedFile to be a CAutoFile wrapper
This refactor allows to forward some calls to the underlying CAutoFile,
instead of re-implementing the logic in the buffered file.

src/validation.cpp
@@ -4577,7 +4577,7 @@ bool Chainstate::LoadGenesisBlock()
 }
 
 void ChainstateManager::LoadExternalBlockFile(
-    FILE* fileIn,
+    CAutoFile& file_in,
     FlatFilePos* dbp,
     std::multimap<uint256, FlatFilePos>* blocks_with_unknown_parent)
 {
@@ -4589,7 +4589,7 @@ void ChainstateManager::LoadExternalBlockFile(
 
     int nLoaded = 0;
     try {
-        BufferedFile blkdat{fileIn, 2 * MAX_BLOCK_SERIALIZED_SIZE, MAX_BLOCK_SERIALIZED_SIZE + 8, CLIENT_VERSION};
+        BufferedFile blkdat{file_in, 2 * MAX_BLOCK_SERIALIZED_SIZE, MAX_BLOCK_SERIALIZED_SIZE + 8};
         // nRewind indicates where to resume scanning in case something goes wrong,
         // such as a block fails to deserialize.
         uint64_t nRewind = blkdat.GetPos();

doged-io/doged

Summary:
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/28483 | core#28483]]
https://github.com/bitcoin/bitcoin/pull/28483/commits/9999b89cd37fb2a23c5ebcd91d9cb31d69375933
Depends on D19150
Test Plan: `ninja all check-all bitcoin-fuzzers bench-bitcoin`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19151

src/validation.cpp
@@ -5900,7 +5900,7 @@ bool Chainstate::LoadGenesisBlock() {
 }
 
 void ChainstateManager::LoadExternalBlockFile(
-    FILE *fileIn, FlatFilePos *dbp,
+    CAutoFile &file_in, FlatFilePos *dbp,
     std::multimap<BlockHash, FlatFilePos> *blocks_with_unknown_parent,
     avalanche::Processor *const avalanche) {
     // Either both should be specified (-reindex), or neither (-loadblock).
@@ -5913,8 +5913,7 @@ void ChainstateManager::LoadExternalBlockFile(
     try {
         // Make sure we have at least 2*MAX_TX_SIZE space in the buffer
         // so any transaction can fit in there.
-        BufferedFile blkdat{fileIn, 2 * MAX_TX_SIZE, MAX_TX_SIZE + 8,
-                            CLIENT_VERSION};
+        BufferedFile blkdat{file_in, 2 * MAX_TX_SIZE, MAX_TX_SIZE + 8};
         // nRewind indicates where to resume scanning in case something goes
         // wrong, such as a block fails to deserialize.
         uint64_t nRewind = blkdat.GetPos();