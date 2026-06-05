bitcoin-ghost/bitcoin-core-ladder-script

refactor: VectorWriter without nVersion
The field is unused, so remove it.

This is also required for future commits.

src/net.cpp
@@ -818,7 +818,7 @@ bool V1Transport::SetMessageToSend(CSerializedNetMsg& msg) noexcept
 
     // serialize header
     m_header_to_send.clear();
-    CVectorWriter{INIT_PROTO_VERSION, m_header_to_send, 0, hdr};
+    VectorWriter{m_header_to_send, 0, hdr};
 
     // update state
     m_message_to_send = std::move(msg);

doged-io/doged

Summary:
Remove unused Make() overload in netmessagemaker.h
This is a partial backport of [[https://github.com/bitcoin/bitcoin/pull/28892 | core#28892]]
https://github.com/bitcoin/bitcoin/pull/28892/commits/fa0ed0794161d937d2d3385963c1aa5624b60d17
https://github.com/bitcoin/bitcoin/pull/28892/commits/66669da4a5ca9edf2a40d20879d9a8aaf2b9e2ee
Depends on D19158
Test Plan: `ninja all check-all bitcoin-fuzzers`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19159

src/net.cpp
@@ -828,7 +828,7 @@ void V1TransportSerializer::prepareForTransport(
 
     // serialize header
     header.reserve(CMessageHeader::HEADER_SIZE);
-    CVectorWriter{INIT_PROTO_VERSION, header, 0, hdr};
+    VectorWriter{header, 0, hdr};
 }
 
 std::pair<size_t, bool> CConnman::SocketSendData(CNode &node) const {