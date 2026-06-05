bitcoin-ghost/bitcoin-core-ladder-script

Use serialization parameters for CAddress serialization
This also cleans up the addrman (de)serialization code paths to only
allow `Disk` serialization. Some unit tests previously forced a
`Network` serialization, which does not make sense, because Bitcoin Core
in production will always `Disk` serialize.
This cleanup idea was suggested by Pieter Wuille and implemented by Anthony
Towns.

Co-authored-by: Pieter Wuille <pieter@wuille.net>
Co-authored-by: Anthony Towns <aj@erisian.com.au>

src/test/fuzz/script_sign.cpp
@@ -36,7 +36,8 @@ FUZZ_TARGET(script_sign, .init = initialize_script_sign)
     const std::vector<uint8_t> key = ConsumeRandomLengthByteVector(fuzzed_data_provider, 128);
 
     {
-        CDataStream random_data_stream = ConsumeDataStream(fuzzed_data_provider);
+        DataStream stream{ConsumeDataStream(fuzzed_data_provider)};
+        CDataStream random_data_stream{stream, SER_NETWORK, INIT_PROTO_VERSION}; // temporary copy, to be removed along with the version flag SERIALIZE_TRANSACTION_NO_WITNESS
         std::map<CPubKey, KeyOriginInfo> hd_keypaths;
         try {
             DeserializeHDKeypaths(random_data_stream, key, hd_keypaths);

doged-io/doged

Summary:
`Network` serialization, which does not make sense, because Bitcoin ABC
This concludes backport of [[https://github.com/bitcoin/bitcoin/pull/25284 | core#25284]]
https://github.com/bitcoin/bitcoin/pull/25284/commits/fac81affb527132945773a5315bd27fec61ec52f
Depends on D19056
Test Plan:
`ninja all check-all bitcoin-fuzzers`
Run the seeder for a while, check there are results (`cat dnsseed.dump`).
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D19057

src/test/fuzz/script_sign.cpp
@@ -36,8 +36,7 @@ FUZZ_TARGET_INIT(script_sign, initialize_script_sign) {
         ConsumeRandomLengthByteVector(fuzzed_data_provider, 128);
 
     {
-        CDataStream random_data_stream =
-            ConsumeDataStream(fuzzed_data_provider);
+        DataStream random_data_stream{ConsumeDataStream(fuzzed_data_provider)};
         std::map<CPubKey, KeyOriginInfo> hd_keypaths;
         try {
             DeserializeHDKeypaths(random_data_stream, key, hd_keypaths);